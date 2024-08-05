51c74f1437db07fc480c47f14472cb00650fa8a7
Jeric Adriano <jeric.adriano@nutricoach.pro>
2024-08-05 21:58:41 +0800

    chore: change pin color

--- src/screens/Home/components/Map.tsx
+++ src/screens/Home/components/Map.tsx
@@ -52,6 +52,7 @@ const Map: React.FC = () => {
     isSelectedRideAccepted && (
       <>
         <Marker
+          pinColor="green"
           coordinate={coordinateFromAddress(selectedRide?.pickupLocation)}
         />
         <Marker coordinate={coordinateFromAddress(selectedRide?.destination)} />
